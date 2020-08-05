615e75c301f83783446c4016d3c2e8f1bf8825fc
zWR417H <69257564+zWR417H@users.noreply.github.com>
2020-08-05 12:40:21 -0400

    Create installme.sh

---
+++ installme.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+printf "						By:- "
+printf "\n"
+printf "			   			   zWR417H"
+printf "\n"
+ping -c 1 -q google.com >&/dev/null;
+
+if [ $? -eq 0 ]; then
+
+	echo "[+] Grab a CUP of Coffee :D"
+	printf "\n"
+	sudo apt update -y
+	sudo apt install git -y
+	sudo apt install tor -y
+
+	sudo git clone https://github.com/Und3rf10w/kali-anonsurf.git
+
+	cd kali-anonsurf
+	sudo chmod +x installer.sh
+	sudo ./installer.sh
+	cd ..
+	sudo chmod +x bin/toripbounce
+	sudo chmod +x bin/stip
+	sudo mv bin/toripbounce /usr/bin/
+	sudo mv bin/stip /usr/bin/
+	sudo rm -rf kali-anonsurf
+
+	printf "\n"
+	echo "[+] All Done Now you can run this with toripbounce commmand !"
+	printf "\n"
+	echo "Warning Please run sudo stip command after you stop toripbounce"
+else
+    echo "[-] Please connect with Internet Connection !"
+fi
